9d3836b638717cc792c318f750cb9a872f59f516
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-03-14 07:24:08 +1100

    installed apollo server and beginning on react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import ApolloProvider from './ApolloProvider'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {ApolloProvider}
   </React.StrictMode>,
   document.getElementById('root')
 );
