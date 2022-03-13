eb56d3b910021bcb323ce1e9b86e3c11756299db
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-03-13 18:16:43 +1100

    Finished server side deployment and beginning React

---
+++ util/check-auth.js
@@ -0,0 +1,30 @@
+const { AuthenticationError } = require("apollo-server");
+
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../config");
+
+module.exports = (context) => {
+  // context = {...headers}
+  const authHeader = context.req.headers.authorization;
+
+  if (authHeader) {
+    // convention: Bearer ...
+    const token = authHeader.split("Bearer ")[1];
+
+    if (token) {
+      try {
+        const user = jwt.verify(token, SECRET_KEY);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError("Invalid / Expired token.");
+      }
+    }
+
+    throw new Error('Authentication token must be \'Bearer [token]')
+
+  } // end of authHeader
+
+  throw new Error('Authorization header must be provided.')
+
+};
